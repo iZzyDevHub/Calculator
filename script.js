7f044dd3b6274f3028bbc5753de1fd6372d76f14
Ishan Mithun <ishanmithun91@gmail.com>
2026-09-16 21:47:25 +0530

    Did some changes in js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let display = document.querySelector('.display');
 let buttons = document.querySelectorAll('.btn');
 let operators = document.querySelectorAll('.operator');
-
+let equalButton = document.querySelector('#equal');
+let clearButton = document.querySelector('#clear');
+let backspaceButton = document.querySelector('#backspace');
+let decimalButton = document.querySelector('#decimal');
